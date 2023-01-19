54c37766e050e2bbd3f2e2e17dc6f2ca34544783
ALEKSANDER <melsmaxms@gmail.com>
2023-01-19 14:27:54 +0700

    First commit

---
+++ src/ofoh.java
@@ -0,0 +1,21 @@
+public class ofoh {
+
+    public static void main(String[] args) {
+
+        int balance = 500;
+        int amount = 1300;
+        int sum = 0;
+        int bonus = 0;
+
+        if (amount > 1000) {
+            bonus = amount / 100;
+            sum = balance + amount + bonus;
+        }
+        else {
+            sum = balance + amount;
+        }
+        System.out.println( "Ваш Бонус: " + bonus);
+        System.out.println( "Ваш Баланс: " + sum);
+    }
+
+}
